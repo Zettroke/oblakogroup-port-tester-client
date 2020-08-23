960544c02b0d64d04e7597adeab7e022bf4e7795
Zettroke <skorostnoy2000@yandex.ru>
2020-08-23 05:45:24 +0300

    add tcp check

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 use serde::Deserialize;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use std::time::Duration;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use structopt::StructOpt;
 use std::path::PathBuf;
 use rayon::prelude::*;
-use std::net::{SocketAddr, UdpSocket, Ipv4Addr};
-use std::io::{Read, Write};
+use std::net::{TcpStream, SocketAddr, UdpSocket, Ipv4Addr};
+use std::io::Write;
 use prettytable::{Attr, Table, Cell, Row, row, cell, color};
-use std::error::Error;
+use serde::export::Formatter;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "port-tester", about = "An example of StructOpt usage.")]
 struct Opt {
     /// manifest url
     #[structopt(short, long)]
-    manifest: String,
+    manifest_url: String,
     /// number of threads
     #[structopt(short, long)]
     jobs: Option<usize>,
     /// output
     #[structopt(parse(from_os_str))]
     out: Option<PathBuf>,
+    /// disables colors in stdout
+    #[structopt(long)]
+    disable_colors: bool,
 }
 
 #[derive(Debug, Deserialize)]
@@ -62,7 +65,22 @@ enum DomainCheckStatus {
 
 struct PortCheckResult {
     port: u16,
+    protocol: PortCheckProtocol,
     status: PortCheckStatus,
+}
+
+enum PortCheckProtocol {
+    TCP,
+    UDP,
+}
+
+impl std::fmt::Display for PortCheckProtocol {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::TCP => f.write_str("TCP"),
+            Self::UDP => f.write_str("UDP")
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -82,25 +100,38 @@ static MSG: [u8; 8] = [1, 7, 12, 74, 123, 96, 255, 11];
 fn main() {
     let conf: Opt = Opt::from_args();
 
-    let resp = ureq::get(&conf.manifest).build().call();
+    if !conf.manifest_url.starts_with("http://") {
+        eprintln!("Not found http scheme in manifest url, please include it!");
+        std::process::exit(-1);
+    }
 
-    let remote_conf: Config = serde_json::from_reader(resp.into_reader()).unwrap();
+    let resp = ureq::get(&conf.manifest_url).build().call();
+    if resp.error() {
+        eprintln!("Manifest fetching error! If endpoint is down contact Oblakogroup");
+        std::process::exit(-1);
+    }
+
+    let remote_conf: Config = serde_json::from_reader(resp.into_reader())
+        .expect("Manifest parsing error! Contact Oblakogroup");
+
     let m = Arc::new(MultiProgress::new());
     let sty = ProgressStyle::default_bar()
         .template("[{elapsed_precise}] {bar:40.yellow/blue} {pos:>7}/{len:7} {msg}")
         .progress_chars("##-");
 
     rayon::ThreadPoolBuilder::new()
-        .num_threads(conf.jobs.unwrap_or_else(|| num_cpus::get()))
-        .build_global().unwrap();
+        .num_threads(conf.jobs.unwrap_or_else(|| num_cpus::get() * 2))
+        .build_global()
+        .unwrap();
 
+    // Parallel processing through rayon
     let entries: Vec<_> = remote_conf.domains.par_iter().map(|entry: &DomainEntry| {
         let address = entry.address.clone();
         match serde_json::from_reader::<_, PortList>(
             ureq::get(&format!("http://{}:1555", address)).call().into_reader()
         ) {
             Ok(pl) => {
-                let pb = m.add(ProgressBar::new(pl.port_list.len() as u64));
+                let pb = m.add(ProgressBar::new(pl.port_list.len() as u64 * 2));
                 pb.set_style(sty.clone());
                 pb.set_message(&entry.url);
                 (entry.url.clone(), address, Some(pl), pb)
@@ -114,6 +145,8 @@ fn main() {
         }
     }).collect::<Vec<_>>();
 
+    // Because we need to call MultiProgress::join to start render which blocks current thread
+    // but rayon blocks main thread too, so we start separate render thread for progress bars.
     let m_clone = m.clone();
     let progress_thread = std::thread::spawn(move || {
         m_clone.join().unwrap();
@@ -126,22 +159,33 @@ fn main() {
             pb.tick();
             let sock_addr = SocketAddr::from((address.parse::<Ipv4Addr>().unwrap(), 0));
 
-            let results: Vec<PortCheckResult> = pl.port_list.par_iter().map(|port| {
-                let mut sock = match std::net::UdpSocket::bind(SocketAddr::from(([0, 0, 0, 0], 0))) {
-                    Ok(sock) => sock,
-                    Err(e) => return PortCheckResult { port: *port, status: PortCheckStatus::Error(PortCheckError::IoError(e)) }
-                };
+            let results: Vec<PortCheckResult> = pl.port_list.par_iter().flat_map(|port| {
+                let port = *port;
                 let mut addr = sock_addr.clone();
-                addr.set_port(*port);
-                if let Err(e) = sock.connect(addr) {
-                    return PortCheckResult { port: *port, status: PortCheckStatus::Error(PortCheckError::IoError(e)) };
-                }
-                let res = check_connection(&mut sock, *port);
+                addr.set_port(port);
+
+                let res_udp = test_udp(addr.clone());
                 pb.inc(1);
 
-                res
+                let res_tcp = match test_tcp(addr) {
+                    Ok(_) => PortCheckResult {
+                        port,
+                        protocol: PortCheckProtocol::TCP,
+                        status: PortCheckStatus::Success,
+                    },
+                    Err(e) => PortCheckResult {
+                        port,
+                        protocol: PortCheckProtocol::TCP,
+                        status: PortCheckStatus::Error(e),
+                    }
+                };
+                pb.inc(1);
+
+                vec![res_udp, res_tcp]
             }).collect();
+
             pb.finish();
+
             DomainCheckReport {
                 url: domain,
                 status: DomainCheckStatus::Alive {
@@ -160,8 +204,9 @@ fn main() {
     }).collect();
     progress_thread.join().unwrap();
 
+    // Fancy output
     let mut table = Table::new();
-    table.add_row(row!["Port", "Status", "Error"]);
+    table.add_row(row!["Port", "Protocol", "Status", "Error"]);
     for domain_report in reports {
         let mut rows = vec![];
         let mut accessible = 0;
@@ -172,39 +217,40 @@ fn main() {
                         accessible += 1;
                     }
                     PortCheckStatus::Error(ref e) => {
-                        let mut err = format!("{:#?}", e);
-                        string_multiline(&mut err);
-                        rows.push(row![format!("{}", port_result.port), "inaccessible", err]);
+                        rows.push(row![
+                            format!("{}", port_result.port),
+                            format!("{}", port_result.protocol),
+                            "inaccessible",
+                            textwrap::fill(&format!("{:#?}", e), 80)
+                        ]);
                     }
                 }
             }
-            // println!("  {} out of {} ports accessible", accessible, port_results.len());
         }
 
         match domain_report.status {
             DomainCheckStatus::Alive { ref port_results } => {
                 let mut cell = Cell::new(
-                    &format!("{} ({} out of {} ports accessible from port range {}:{})",
+                    &format!("{} ({} out of {} ports from the range {}:{} are accessible)",
                              domain_report.url,
                              accessible,
                              port_results.len(),
                              domain_report.port_range.min,
                              domain_report.port_range.max
                     )
-                ).with_hspan(3);
-                if accessible == port_results.len() {
+                ).with_hspan(4);
+                if accessible == port_results.len() && !conf.disable_colors {
                     cell = cell.with_style(Attr::BackgroundColor(color::GREEN));
                 }
                 table.add_row(Row::new(vec![cell]));
             }
             DomainCheckStatus::Dead => {
-                table.add_row(
-                    Row::new(vec![
-                        Cell::new(&format!("{} (dead)", domain_report.url))
-                            .with_style(Attr::BackgroundColor(color::RED))
-                            .with_hspan(3)
-                    ])
-                );
+                let mut cell = Cell::new(&format!("{} (dead)", domain_report.url))
+                    .with_hspan(4);
+                if !conf.disable_colors {
+                    cell = cell.with_style(Attr::BackgroundColor(color::RED))
+                }
+                table.add_row(Row::new(vec![cell]));
             }
         };
 
@@ -214,18 +260,37 @@ fn main() {
     }
     table.printstd();
 
-    table.row_iter_mut().for_each(|r| r.iter_mut().for_each(|c| c.reset_style()));
     if let Some(path) = conf.out {
+        table.row_iter_mut().for_each(|r| r.iter_mut().for_each(|c| c.reset_style()));
+
         let mut out_file = std::fs::File::create(path).unwrap();
-        table.print(&mut out_file);
+        table.print(&mut out_file).unwrap();
     }
     print!("Enter something to exit...");
-    std::io::stdout().flush();
+    std::io::stdout().flush().unwrap();
     let mut s = String::new();
-    std::io::stdin().read_line(&mut s);
+    std::io::stdin().read_line(&mut s).unwrap();
 }
 
-fn check_connection(sock: &mut UdpSocket, port: u16) -> PortCheckResult {
+fn test_udp(addr: SocketAddr) -> PortCheckResult {
+    let port = addr.port();
+    let sock = match UdpSocket::bind(SocketAddr::from(([0, 0, 0, 0], 0))) {
+        Ok(sock) => sock,
+        Err(e) => return PortCheckResult {
+            port,
+            protocol: PortCheckProtocol::UDP,
+            status: PortCheckStatus::Error(PortCheckError::IoError(e)),
+        }
+    };
+
+    if let Err(e) = sock.connect(addr) {
+        return PortCheckResult {
+            port,
+            protocol: PortCheckProtocol::UDP,
+            status: PortCheckStatus::Error(PortCheckError::IoError(e)),
+        };
+    }
+
     let mut error_sending = false;
     let mut error_receiving = false;
     let mut curr_res: PortCheckStatus = PortCheckStatus::Success;
@@ -287,28 +352,17 @@ fn check_connection(sock: &mut UdpSocket, port: u16) -> PortCheckResult {
         }
     }
 
-    PortCheckResult { port, status: curr_res }
+    PortCheckResult {
+        port,
+        protocol: PortCheckProtocol::UDP,
+        status: curr_res,
+    }
 }
 
-fn string_multiline(s: &mut String) {
-    let mut line_len = 0;
-    let mut i = 0;
-    let mut last_space_ind = 0;
-    while i < s.len() {
-        if s.as_bytes().get(i).map_or(false, |v| *v == b'\n' || *v == b'\r') {
-            line_len = 0;
-            last_space_ind = 0;
-        } else {
-            line_len += 1;
-            if line_len > 100 && last_space_ind != 0 {
-                s.insert(last_space_ind, '\n');
-                line_len = 0;
-                last_space_ind = 0;
-            }
-        }
-        if s.as_bytes().get(i).map_or(false, |v| *v == b' ') {
-            last_space_ind = i;
-        }
-        i += 1;
-    }
+fn test_tcp(addr: SocketAddr) -> Result<(), PortCheckError> {
+    TcpStream::connect_timeout(&addr, Duration::from_secs(5))
+        .map_err(|e| PortCheckError::IoError(e))
+        ?;
+
+    Ok(())
 }
